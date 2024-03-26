521f56f8c05abf0ff2ebd0cdb9cb57f95c7a55ff
Haytham Idrees <haythamidrees9@gmail.com>
2024-03-26 19:54:10 +0700

    deploy recipe page

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5G4FBIOF.mjs";import"./chunk-LCFON5P3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3DTFSFQK.mjs";import"./chunk-LCFON5P3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
